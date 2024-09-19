7176cf59f6dcb3e63db34994ee2464c9158ab63a
= mlm-games <= gerrit.r6a6a@passinbox.com>
2024-09-19 13:03:12 +0530

    disable mtp and try

--- omni_thor5pro.mk
+++ omni_thor5pro.mk
@@ -4,6 +4,10 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Charger Mode
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := thor5pro
 PRODUCT_NAME := omni_thor5pro
